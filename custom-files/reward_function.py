1628140381ea50eb298365ad34a136ba0876b104
Amy2814154 <harshithamrutur@gmail.com>
2024-07-02 11:00:12 +0530

    Update reward_function.py

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -9,12 +9,12 @@ def reward_function(params):
     steering = abs(params['steering_angle'])
     speed = params['speed']
 
-    # Calculate 3 marks that are farther and farther away from the center line
+    # Calculate markers that are farther away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
 
-    # Give higher reward if the car is closer to center line and vice versa
+    # Reward based on distance from center
     if distance_from_center <= marker_1:
         reward = 1.0
     elif distance_from_center <= marker_2:
@@ -22,18 +22,18 @@ def reward_function(params):
     elif distance_from_center <= marker_3:
         reward = 0.1
     else:
-        reward = 1e-3  # likely crashed/ close to off track
+        reward = 1e-3  # likely crashed/close to off track
 
     # Steering penalty threshold
     ABS_STEERING_THRESHOLD = 15
 
-    # Penalize reward if the car is steering too much
+    # Penalize if the car is steering too much
     if steering > ABS_STEERING_THRESHOLD:
         reward *= 0.7
 
-    # Reward higher speeds more significantly
-    SPEED_THRESHOLD = 2.0
+    # Significantly reward higher speeds
+    SPEED_THRESHOLD = 3.0
     if speed > SPEED_THRESHOLD:
-        reward *= 1.5
+        reward *= 2.0
 
     return float(reward)
